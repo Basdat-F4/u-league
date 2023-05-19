35cead9da31d48174a8721df595d4f497045270a
Rania Maharani Narendra <rania.maharani11@ui.ac.id>
2023-05-19 11:39:03 +0700

    loginnya error trs gmn dah

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
-from authentication.views import homepage;
+from django.urls import path
+from authentication.views import homepage, login;
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('', homepage, name='login-and-register')
+    path('', homepage, name='login-and-register'),
+    path('login/', login, name='login'),
+
 ]
