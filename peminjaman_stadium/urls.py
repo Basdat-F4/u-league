bc07819db5a04b0e36b4c8172354168a92a668ec
Naufal Weise <naufal.weise@ui.ac.id>
2023-05-25 18:12:07 +0700

    Add edit peminjaman

--- peminjaman_stadium/urls.py
+++ peminjaman_stadium/urls.py
@@ -6,4 +6,6 @@ app_name = 'peminjaman_stadium'
 urlpatterns = [
  path('list', get_list_peminjaman, name='get_list_peminjaman'),
  path('create', create_peminjaman, name='create_peminjaman'),
+ path('form/<str:mode>', view_peminjaman_form, name='view_peminjaman_form'),
+ path('edit', edit_peminjaman, name='edit_peminjaman'),
 ]
--- peminjaman_stadium/views.py
+++ peminjaman_stadium/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.views.decorators.http import require_POST, require_GET
 from django.shortcuts import render, redirect
 from utils.query import query
 from datetime import datetime, date, time
@@ -8,24 +7,15 @@ from datetime import datetime, date, time
 def get_list_peminjaman(request):
     username = request.session["username"]
     list_peminjaman = query(f"""
-        SELECT start_datetime, end_datetime, s.nama as stadium FROM peminjaman 
+        SELECT start_datetime, end_datetime, s.nama as stadium, id_stadium FROM peminjaman 
         JOIN manajer using (id_manajer)
         JOIN stadium s using (id_stadium) 
         WHERE username='{username}'
         """)
     return render(request, "peminjaman_stadium/peminjaman-stadium.html", {"list_peminjaman": list_peminjaman})
 
+@require_POST
 def create_peminjaman(request):
-    if request.method != "POST":
-        list_stadium = query(f"SELECT * FROM stadium")
-        list_waktu = []
-        id_stadium = request.GET.get('id_stadium')
-        date = request.GET.get('date')
-        if id_stadium is not None and date is not None :
-            list_waktu = get_list_waktu(id_stadium, date)
-
-        return render(request, 'peminjaman_stadium/peminjaman-stadium-form.html', {"list_stadium": list_stadium, "list_waktu": list_waktu, "id_stadium": id_stadium, "book_date": date})
-
     username = request.session["username"]
     id_manajer = query(f"SELECT id_manajer FROM manajer WHERE username='{username}'")[0][0]
     start_datetime = str(request.POST["start_datetime"])
@@ -34,6 +24,35 @@ def create_peminjaman(request):
     insert_response = query(f"INSERT INTO peminjaman VALUES ('{id_manajer}', '{start_datetime}', '{end_datetime}', '{id_stadium}')")
     print(insert_response)
     return redirect("/peminjaman_stadium/list")
+
+
+@require_GET
+def view_peminjaman_form(request, mode):
+    list_stadium = query(f"SELECT * FROM stadium")
+    list_waktu = []
+    id_stadium = request.GET.get('id_stadium')
+    date = request.GET.get('date')
+    initial_datetime = request.GET.get("initial_datetime")
+    if id_stadium is not None and date is not None :
+        list_waktu = get_list_waktu(id_stadium, date)
+
+    submission_url = "/peminjaman_stadium/" + mode
+        
+    return render(request, 'peminjaman_stadium/peminjaman-stadium-form.html', {"list_stadium": list_stadium, "list_waktu": list_waktu, "id_stadium": id_stadium, "book_date": date, "submission_url": submission_url, "initial_datetime": initial_datetime})
+
+
+@require_POST
+def edit_peminjaman(request):
+    username = request.session["username"]
+    id_manajer = query(f"SELECT id_manajer FROM manajer WHERE username='{username}'")[0][0]
+    start_datetime = str(request.POST["start_datetime"])
+    initial_datetime = str(request.POST["initial_datetime"])
+    end_datetime = str(request.POST["end_datetime"])
+    id_stadium = str(request.POST["id_stadium"])
+    update_response = query(f"UPDATE peminjaman SET start_datetime='{start_datetime}', end_datetime='{end_datetime}', id_stadium='{id_stadium}' WHERE id_manajer='{id_manajer}' AND start_datetime='{initial_datetime}'")
+    print(update_response)
+    return redirect("/peminjaman_stadium/list")
+
     
 
 def get_list_waktu(id_stadium, datestr):
