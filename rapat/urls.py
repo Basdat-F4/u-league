ae1883d55d129d49fa5fce80bedc9f7bb94e24d4
Naufal Weise <naufal.weise@ui.ac.id>
2023-05-25 09:50:10 +0700

    Add views mulai rapat

--- rapat/urls.py
+++ rapat/urls.py
@@ -5,4 +5,5 @@ app_name = 'rapat'
 
 urlpatterns = [
  path('history-rapat', get_history, name='get_history'),
+ path('mulai-rapat', start_rapat, name='mulai_rapat'),
 ]
--- rapat/views.py
+++ rapat/views.py
@@ -21,3 +21,21 @@ def get_history(request):
     context = {"list_rapat": res}
 
     return render(request, "history_rapat.html", context)
+
+def start_rapat(request):
+    username = request.session["username"]
+    id_manajer = query(f"SELECT id_manajer FROM MANAJER WHERE username = '{username}'")
+    list_pertandingan = query(f"""
+            SELECT DISTINCT ON (P.id_pertandingan) P.id_pertandingan, A.nama_tim as tim_a, B.nama_tim as tim_b, S.nama as stadium, concat(TO_CHAR(P.start_datetime::timestamp::date, ' DD Month YYYY'), ', ', TO_CHAR(start_datetime,'HH:MI'), ' - ', TO_CHAR(end_datetime,'HH:MI')) as display  
+            FROM PERTANDINGAN P, TIM_PERTANDINGAN A, TIM_PERTANDINGAN B, STADIUM S
+            WHERE P.id_pertandingan = A.id_pertandingan
+            AND P.id_pertandingan = B.id_pertandingan
+            AND S.id_stadium = P.stadium
+            AND A.nama_tim != B.nama_tim
+            GROUP BY P.id_pertandingan, A.nama_tim, B.nama_tim, S.nama;
+            """)
+    context = {"list_pertandingan": list_pertandingan}
+    print(list_pertandingan)
+    
+
+    return render(request, "mulai-rapat.html", context)
